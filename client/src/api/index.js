b8fc26aed06dc9691c3a7890f39813ad5d099c87
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2022-10-27 19:32:01 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -10,7 +10,7 @@ API.interceptors.request.use((req) => {
   return req;
 });
 
-export const fetchPosts = () => API.get('/posts');
+export const fetchPosts = () => API.get('/posts'); //fetching posts
 export const createPost = (newPost) => API.post('/posts', newPost);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
