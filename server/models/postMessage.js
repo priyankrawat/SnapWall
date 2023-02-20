b035270de9c4f1a8f5293031f7dd448779668dfb
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2023-02-20 13:30:00 +0530

    Update postMessage.js

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const postSchema = mongoose.Schema({
-    title: String,
+    title: String, //can change this to varchar
     message: String,
     name: String,
     creator: String,
