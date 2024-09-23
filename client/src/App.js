3d80ba3d2d4152af3743f8d9c618fbc4519a668e
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2024-09-23 11:29:27 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Auth from './components/Auth/Auth';
-
+ 
 const App = () => (   
   <BrowserRouter> 
     <Container maxWidth="lg"> 
