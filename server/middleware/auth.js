34cca62ea78f07a698d01bbe809471bb24e8d78c
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2024-11-10 10:46:22 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -11,7 +11,7 @@ const auth = async (req, res, next) => {
 
     if (token && isCustomAuth) {      
       decodedData = jwt.verify(token, secret); // storing jwt secret
-
+ 
       req.userId = decodedData?.id;
     } else {
       decodedData = jwt.decode(token);
